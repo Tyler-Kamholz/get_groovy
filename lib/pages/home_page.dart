d2cdae4151ce830b61e7a6223ec534ae631b2e04
MKotras7 <mkotras7@gmail.com>
2022-10-20 23:40:49 -0500

    Use standard post card

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:getgroovy/widgets/post_card_builder.dart';
 import 'package:scroll_app_bar/scroll_app_bar.dart';
 
 class HomePage extends StatefulWidget {
@@ -20,19 +21,7 @@ class _HomePageState extends State<HomePage> {
         child: ListView.builder(
           controller: controller,
           itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              child: ListTile(
-                leading: const Text('Album Cover'),
-                title: const Text('this is where the songs will go'),
-                subtitle: const Text('add to playlist goes here  with preview'),
-                contentPadding:
-                    const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-                tileColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-              ),
-            );
+            return PostCardBuilder.buildPostCard();
           },
         ),
       ),
