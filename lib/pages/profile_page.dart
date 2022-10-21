0c7163064c1a7e49405b5a1b967bb2d585852799
MKotras7 <mkotras7@gmail.com>
2022-10-20 23:41:06 -0500

    Rework profile page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,75 +1,176 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:scroll_app_bar/scroll_app_bar.dart';
+import 'package:getgroovy/helpers/helpers.dart';
+
+import '../widgets/post_card_builder.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
+
+  static const favStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+
+  static const thingStyle = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 24,
+    color: Colors.black,
+  );
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
+enum _profileTabs { following, posts, followers }
+
 class _ProfilePageState extends State<ProfilePage> {
+  _profileTabs _currentTab = _profileTabs.posts;
   final controller = ScrollController();
+  late final outerListChildren = <Widget>[
+    SliverToBoxAdapter(
+      child: Column(
+        children: [
+          buildAvatar(),
+          buildName(),
+          buildButtonBar(),
+          const Divider(),
+        ],
+      ),
+    ),
+    buildSliverList(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Center(
-            child: Container(
-              color: Colors.black,
-              width: 200,
-              height: 200,
-              margin: const EdgeInsets.all(20),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 20, left: 20, right: 20),
-            child: Container(
-              color: Colors.green,
-              width: 200,
-              height: 20,
-              child: const Text(
-                textAlign: TextAlign.center,
-                '@User_Name',
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: SizedBox(
-              //color: Colors.blue,
-              width: 400,
-              height: 400,
-              child: Snap(
-                controller: controller.appBar,
-                child: ListView.builder(
-                  controller: controller,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 10, horizontal: 20),
-                      child: ListTile(
-                        leading: const Text('Album Cover'),
-                        title: const Text('this is where the songs will go'),
-                        subtitle:
-                            const Text('add to playlist goes here  with preview'),
-                        contentPadding: const EdgeInsets.symmetric(
-                            vertical: 15, horizontal: 30),
-                        tileColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-          )
-        ],
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: buildSliverLister(),
+        ),
       ),
     );
   }
+
+  List<Widget> buildSliverLister() {
+    return <Widget>[
+      SliverToBoxAdapter(
+        child: Column(
+          children: [
+            Padding(
+                padding: const EdgeInsets.fromLTRB(0, 20, 0, 10),
+                child: buildAvatar()),
+            buildName(),
+            buildButtonBar(),
+            const Divider(),
+          ],
+        ),
+      ),
+      buildSliverList(),
+    ];
+  }
+
+  Widget buildAvatar() {
+    return CircleAvatar(
+        backgroundColor: ColorHelper.random(), minRadius: 100, maxRadius: 100);
+  }
+
+  Widget buildName() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20, left: 20, right: 20),
+      child: Text('Bob Robertson',
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          )),
+    );
+  }
+
+  Widget buildButtonBar() {
+    return IntrinsicHeight(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        createStackedbutton(512, 'Following', () {
+          setState(() {
+            _currentTab = _profileTabs.followers;
+          });
+        }),
+        const VerticalDivider(),
+        createStackedbutton(1024, 'Posts', () {
+          setState(() {
+            _currentTab = _profileTabs.posts;
+          });
+        }),
+        const VerticalDivider(),
+        createStackedbutton(128, 'Followers', () {
+          setState(() {
+            _currentTab = _profileTabs.following;
+          });
+        }),
+      ],
+    ));
+  }
+
+  Widget createStackedbutton(int count, String context, Function onClick) {
+    return TextButton(
+        onPressed: () {
+          onClick();
+        },
+        child: Column(
+          children: [
+            Text(
+              count.toString(),
+              style: ProfilePage.favStyle,
+            ),
+            Text(
+              context,
+              style: ProfilePage.favStyle,
+            ),
+          ],
+        ));
+  }
+
+  SliverList buildSliverList() {
+    switch (_currentTab) {
+      case _profileTabs.followers:
+        return buildUserList();
+      case _profileTabs.following:
+        return buildUserList();
+      case _profileTabs.posts:
+        return buildPostList();
+    }
+  }
+
+  SliverList buildUserList() {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) => ListTile(
+          leading: CircleAvatar(
+              backgroundColor: ColorHelper.random(), minRadius: 20, maxRadius: 20),
+          title: Text('Username@${Random().nextInt(8000) + 1000}'),
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          onTap: () {},
+        ),
+        childCount: 15,
+      ),
+    );
+  }
+
+  SliverList buildPostList() {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) => PostCardBuilder.buildPostCard(),
+        childCount: 15,
+      ),
+    );
+  }
+
+  
+
+  
 }
