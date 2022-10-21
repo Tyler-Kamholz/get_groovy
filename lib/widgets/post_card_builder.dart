9923edd0b1bbcbfc4601dcb0987e542e914abfe2
MKotras7 <mkotras7@gmail.com>
2022-10-20 23:37:56 -0500

    Create post card builder class and function to assist with creating a standard Card for displaying posts

---
+++ lib/widgets/post_card_builder.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../pages/helpers/helpers.dart';
+
+class PostCardBuilder {
+  static Card buildPostCard() {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      elevation: 10,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(children: [
+          Container(
+            width: 100.0,
+            height: 100.0,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+              color: ColorHelper.random(),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(16, 0, 0, 0),
+            child: Text(
+              "Song name",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
